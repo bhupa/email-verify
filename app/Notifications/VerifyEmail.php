1469816317dfe007320e632ec330af5eb79b7b4c
Bhupendra Rice <bhupendra.rice@gmail.com>
2021-11-29 17:59:33 +0545

    email verify

---
+++ app/Notifications/VerifyEmail.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\VerifyEmail as VerifyEmailBase;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+
+class VerifyEmail extends VerifyEmailBase
+{
+    public $name;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+   
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        if (static::$toMailCallback) {
+            return call_user_func(static::$toMailCallback, $notifiable);
+        }
+        return (new MailMessage)
+            ->subject(Lang::getFromJson('Verify Email Address'))
+            ->line(Lang::getFromJson('Please click the button below to verify your email address.'))
+            ->action(
+                Lang::getFromJson('Verify Email Address'),
+                $this->verificationUrl($notifiable)
+            )
+            ->line(Lang::getFromJson('If you did not create an account, no further action is required.'));
+    }
+}
